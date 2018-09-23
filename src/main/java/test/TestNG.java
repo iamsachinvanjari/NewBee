acdf236c02a301a6699c8a3f221599ad33e81f96
iamsachinvanjari <iamsachin.vanjari@gmail.com>
2018-09-23 00:54:34 -0700

    first commit

---
+++ src/main/java/test/TestNG.java
@@ -0,0 +1,12 @@
+package test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class TestNG {
+
+	@BeforeTest
+	public void Test()
+	{
+		System.out.println("truye");
+	}
+	
+}
